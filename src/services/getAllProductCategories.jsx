971429636bde0114e4a5e08e247fc43f83ece75d
diandraananda <diandradwiananda@gmail.com>
2024-11-17 20:55:48 +0700

    first commit

---
+++ src/services/getAllProductCategories.jsx
@@ -0,0 +1,25 @@
+const getAllProductCategories = ()  => {
+  return [   {
+    id: 'FMEN',
+    slug: 'men-bag',
+    name: `Men's Bag`
+  },
+  {
+    id: 'FWMEN',
+    slug: 'women-bag',
+    name: `Women's Bag`
+  },
+  {
+    id: 'FWMENseller',
+    slug: 'best seller-bag',
+    name: `Best Selling`
+  },
+  {
+    id: 'FWMENNEW',
+    slug: 'New arrival-bag',
+    name: `New Arrivals`
+  },
+  ]
+}
+
+export default getAllProductCategories
---
+++ src/services/getAllProducts.js
@@ -0,0 +1,110 @@
+export default function getAllProducts() {
+  return [
+    {
+      id: 'Vivienne-west-wood-Ch-maroon-womens',
+      slug: 'Vivienne-west-wood-Chancery-Heart-Bags-womens',
+      name: "Vivienne Westwood Chancery Heart Maroon Leather Bags",
+      category: `Women's Bag`,
+      categorySlug: `women-bag`,
+      price: 5_962_650,
+      stock: 120,
+      imageUrl: '/assets/images/charry heart bags maroon.jpg',
+      description: `Bag shows light wear throughout, mainly to lining, also there is one tiny scuff to the top near the zip, couple scuffs to the bottom and few to the edges of the bag, also there is some scratches and discolour to metal parts.`
+    },
+
+    {
+      id: 'Vivienne-west-wood-ch-black-womens',
+      slug: 'Vivienne-west-wood-Chancery-Heart-Bags-black-womens',
+      name: "Vivienne Westwood Chancery Heart Black Vegan Leather Bags",
+      category: `Women's Bag`,
+      categorySlug:`best seller-bag`,
+      price: 5_962_650,
+      stock: 5,
+      imageUrl: `/assets/images/charry heart bags black.jpeg`,
+      description: `Bag shows wear throughout, mainly to lining (a bit stained), metal parts (scratches and discolour) and wear to the edges.`
+    },
+    {
+      id: 'Vivienne-west-wood-crossbody-womens',
+      slug: 'Vivienne-west-wood-leather-cross-body-bag-womes',
+      name: "Vivienne Westwood Leather Cross Body Bag",
+      category: `Women's Bag`,
+      categorySlug: `women-bag`,
+      price: 8_093_000,
+      stock: 0,
+      imageUrl: `/assets/images/cross body bag.jpeg`,
+      description: `The Louise Heart Crossbody bag is made from patent leather reminiscent of our iconic boutique, now known as Worlds End and shaped into a heart silhouette.`
+    },
+    {
+      id: 'Vivienne-west-wood-anna-mens',
+      slug: 'Vivienne-west-wood-Anna-Camera-Bags-black-womens',
+      name: "Vivienne Westwood Anna Camera Bags",
+      category: `Men's Bag`,
+      categorySlug: `men-bag`,
+      price: 6_605_980,
+      stock: 5,
+      imageUrl: `/assets/images/anna camera bag.jpg`,
+      description: `Our Anna Camera bag returns this season with a soft PU leather silhouette, complete with an all-over grained finish.he piece revives our signature 'Line Orb' 
+      motif on the front panel in silver-tone brass, embodying Vivienne's vision of launching tradition into the future.`
+    },
+    {
+      id: 'Vivienne-west-wood-sns-mens',
+      slug: 'Vivienne-west-wood-squire-now-square-mens',
+      name: "Vivienne Westwood Squire Now Square Crossbody Bags",
+      category: `Men's Bag`,
+      categorySlug: `men-bag`,
+      price: 4_147_625,
+      stock: 100,
+      imageUrl: '/assets/images/squire new square.jpg',
+      description: `Made from grained PU leather, the Squire New Square Crossbody bag opens to a single compartment lined in 
+      recycled cotton fabric and features our orb motif on the front`
+    },
+    {
+      id: 'Vivienne-west-wood-myh-womens',
+      slug: 'Vivienne-west-wood-archive-mini-yasmine-womens',
+      name: "Vivienne Westwood Archive Mini Yasmine Handbag Bags",
+      category: `Women's Bag`,
+      categorySlug: `women-bag`,
+      price: 6_687_600,
+      stock: 100,
+      imageUrl: '/assets/images/archive myh.jpg',
+      description: `Our iconic Archive Mini Yasmine handbag, a new development of the houses classic Yasmine style, 
+      was first presented at the Gold Label Autumn-Winter 1987/88 Time Machine runway show.`
+    },
+    {
+      id: 'Vivienne-west-wood-ch-womens',
+      slug: 'Vivienne-west-wood-archive-chain-womens',
+      name: "Vivienne Westwood Archive Chain Handbag",
+      category: `Women's Bag`,
+      categorySlug: `women-bag`,
+      price: 8_947_900,
+      stock: 100,
+      imageUrl: '/assets/images/archive ch.jpg',
+      description: `First appearing in Vivienne Westwood's 'Cut, Slash & Pull' Spring-Summer collection from 1991, 
+      the Archive Chain handbag is made from shiny, patent leather, complete with a croc-embossed texture.`
+    },
+    {
+      id: 'Vivienne-west-wood-teddy-mens',
+      slug: 'Vivienne-west-wood-teddy-mens',
+      name: "Vivienne Westwood Teddy Tote Bag",
+      category: `Men's Bag`,
+      categorySlug: `men-bag`,
+      price: 11_268_730,
+      stock: 50,
+      imageUrl: '/assets/images/Teddy tote bag.jpg',
+      description: `The Teddy tote bag in soft PU leather features a top-handle silhouette, complete with extendable gussets on either side of the design. The piece receives our archive 'Rubbish' motif, a collage of packaging and 
+      cardboard images - evoking the concept of upcycling newspapers by using them to wrap valuable ornaments when moving house.`
+    },
+    {
+      id: 'Vivienne-west-wood-teddy-unisex',
+      slug: 'Vivienne-west-wood-teddy-unisex',
+      name: "Vivienne Westwood TCarrie Camera Bag",
+      category: `Unisex`,
+      categorySlug: `New arrival-bag`,
+      price: 4_168_870,
+      stock: 50,
+      imageUrl: '/assets/images/Carrie bag.jpg',
+      description: `The design of our Carrie Camera bag takes inspiration from the house's signature shopping bags, distributed in our boutiques during the Red Label era. The piece features a printed leather silhouette, 
+      adorned with the archival 'Arm and Cutlass' branding along the interior panel, complete with metal hardware in a silver-tone finish.`
+    },
+  ]
+}
